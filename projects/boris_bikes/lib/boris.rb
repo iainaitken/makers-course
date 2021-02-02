c7fe53e152f89b656715e666ed6c5f64ba0d700d
iainaitken <iain_aitken@yahoo.co.uk>
2021-02-02 17:11:37 +0000

    update boris bike project with third passing test

--- projects/boris_bikes/lib/boris.rb
+++ projects/boris_bikes/lib/boris.rb
@@ -3,13 +3,14 @@ class Bike
   def working?
 
   end
-  
+
 end
 
 class DockingStation
 
   def release_bike
-
+    bike = Bike.new
+    return bike
   end
 
 end
--- projects/boris_bikes/spec/docking_station_spec.rb
+++ projects/boris_bikes/spec/docking_station_spec.rb
@@ -6,6 +6,8 @@ describe DockingStation do
     expect(DockingStation.new).to respond_to(:release_bike)
   end
 
-
+  it 'is a bike' do
+    expect(DockingStation.new.release_bike).to be_a(Bike)
+  end
 
 end
